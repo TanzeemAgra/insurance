fc65d510192905d53ae069ae1325cc921bfcc1e7
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-08-23 16:02:38 +0530

    Third Push - load_and_save

---
+++ src/load_data.py
@@ -0,0 +1,25 @@
+from distutils.command.config import config
+import os
+from src.get_data import read_params
+import yaml
+import pandas as pd
+import numpy as np
+import argparse
+from pkgutil import get_data
+import configparser
+from get_data import read_params, get_data
+
+def load_and_save(config_path):
+    config=read_params(config_path)
+    df=get_data(config_path)
+    new_cols=[col.replace(" ","_") for col in df.columns]
+    print(new_cols)
+
+
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args=args.parse_args()
+    load_and_save(config_path=parsed_args.config)
