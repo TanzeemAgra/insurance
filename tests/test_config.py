17c639097a98724c73ebb9cd978f98326bdfc9bb
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-08-24 15:56:34 +0530

    ci-cd pipeline

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ import os
 import joblib
 import pytest
 #from prediction_service.prediction import form_response, api_response
-#import prediction_service
+import prediction_service
 
 def test_generic():
     a=3
